30dac43cf0701ac26e0e7472fcda2a5647522cd8
sunnyshrestha <dev.suncha@gmail.com>
2014-11-11 14:46:28 +0545

    Revised customcursoradapter

--- app/src/main/java/dev/suncha/shareloc/CustomCursorAdapter.java
+++ app/src/main/java/dev/suncha/shareloc/CustomCursorAdapter.java
@@ -15,11 +15,14 @@ import java.util.ArrayList;
 /**
  * Created by Sunny on 11/7/2014.
  * refer this http://www.mysamplecode.com/2012/07/android-listview-cursoradapter-sqlite.html added on 11/10/2014
+ * why not working
  */
 public class CustomCursorAdapter extends CursorAdapter {
+    private Cursor cursor;
 
-    public CustomCursorAdapter(Context context, ArrayList c) {
-        super(context, (Cursor) c);
+    public CustomCursorAdapter(Context context, Cursor cur) {
+        super(context, cur);
+        cursor = cur;
     }
 
     @Override
@@ -27,20 +30,32 @@ public class CustomCursorAdapter extends CursorAdapter {
 
         LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
         View retView = inflater.inflate(R.layout.single_row_item,viewGroup,false);
-
+        bindView(retView,context,cursor);
         return retView;
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
 
-//        TextView tvTitle = (TextView)view.findViewById(R.id.tvTitle);
-//        tvTitle.setText(cursor.getString(cursor.getColumnIndex(cursor.getColumnName(1))));
-//
-//        TextView tvDescription = (TextView)view.findViewById(R.id.tvDescription);
-//        tvDescription.setText(cursor.getString(cursor.getColumnIndex(cursor.getColumnName(2))));
-        //int titleCol = cursor.getColumnIndex()
+        TextView tvTitle = (TextView)view.findViewById(R.id.tvTitle);
+        tvTitle.setText(cursor.getString(cursor.getColumnIndex(cursor.getColumnName(1))));
+
+        TextView tvDescription = (TextView)view.findViewById(R.id.tvDescription);
+        tvDescription.setText(cursor.getString(cursor.getColumnIndex(cursor.getColumnName(2))));
+
 //http://www.android86.com/android-advanced/android-sqlite-database-cursoradapter/ perfect
 
+    }
+
+    public int getCount(){
+        return cursor.getCount();
+    }
+
+    public Object getItem(int position) {
+        return position;
+    }
+
+    public long getItemId(int position) {
+        return position;
     }
 }
